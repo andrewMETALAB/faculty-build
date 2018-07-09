8b5fffe73ff0b19ed57c9f1cac062f28c49cefe7
mtaslagyan <mtaslagyan@yahoo.com>
2018-07-09 12:17:22 -0700

    initial draft of structure using bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    
     <router-view/>
   </div>
 </template>
@@ -12,12 +12,87 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{box-sizing: border-box;}
+header{
+    background:white;
+    width:100%;
+    height:45px;
+}
+.homeimg{
+    height:297px;
+    width:100%;
+    background-color:#D22030;
+}
+.container{
+    width: 100%;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.tab-container{
+    border-bottom:solid 0.3px rgb(236, 236, 236);
+}
+
+.border-line-wrapper{
+    border-bottom:solid 0.3px rgb(236, 236, 236);
+    padding-bottom: 1rem;
+    margin-bottom:1rem;
+}
+.wrapper{
+    padding-top:40px;
+}
+h1{
+    font-size: 30px;
+    font-weight:300;
+    color:rgb(75, 75, 75);
+}
+h2{
+    font-size:20px;
+    font-weight:300;
+    color:rgb(75, 75, 75);
+}
+h6{
+    /* font-size:15px; */
+    font-weight:500;
+    color:rgb(75, 75, 75);
+
+}
+.nav-link{
+    padding: .5rem 1.4rem;
+    color:grey;
+}
+ul{
+    text-decoration: none;
+    list-style-type: none;
+    margin:0;
+    padding:0;
+    color:grey;
+    font-weight:300;
+}
+p{
+    color:grey;
+    font-weight:300;
+}
+.card{
+    text-align:center;
+}
+
+footer{
+    height:100px;
+    background:silver;
+    width:100%;
+    margin-top:5rem;
+}
+.connection-links a{
+    border:solid 1px grey;
+    padding:1rem 2rem;
+    margin:0;
+    
+    
+    text-align: center;
+    color:grey;
+    margin:10px 0;
+    display: inline-block;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,86 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+
+
+<div>
+    
+        <!-- header is white bar at top -->
+        <header></header> 
+        <!-- for now keep this div that contains the image element, since its placeholder, later just make the class part of the image so the properties will apply to it directly. -->
+        <div class="homeimg"> 
+            <img src="#" alt="currently placeholder, but will be image of csun or other."> 
+        </div>
+
+        <div class="tab-container">
+            <div class="container">
+                <nav class="nav">
+                    <a class="nav-link active" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
+                </nav>
+            </div>
+        </div>
+        <div class="container">
+            <div class="wrapper">
+                <div class="row">
+                        
+                    <div class="col-3">
+                        <div class="border-line-wrapper">
+                            <h6>CONTACT</h6>
+                            <ul>
+                                <li> <a href="#"></a>steven.fitzgerald@csun.edu</li>
+                                <li> <a href="#"></a>http://www.csun.edu/~steve</li>
+                                <li> <a href="#"></a>818-677-4655</li>
+                                <li> <a href="#"></a>META+LAB: 9423 Reseda Blvd</li>
+                            </ul>
+                        </div>
+                        <div class="border-line-wrapper">
+                            <h6>DEGREES</h6>
+                            <ul class="timeline">
+                                <li class="timeline__header"><strong>D.Sc.</strong> 1995, Cool University</li>
+                                <li class="timeline__header"><strong>M.S.</strong> 1990, Coolest University</li>
+                                <li class="timeline__header"><strong>B.S.</strong> 1986, Coolio University</li>
+                            </ul>
+                        </div>
+                        <div class="border-line-wrapper">
+                            <h6>EXPERTISE</h6>
+                            <h6><span class="badge badge-secondary">New</span></h6>
+                            <h6><span class="badge badge-secondary">New</span></h6>
+                            <h6><span class="badge badge-secondary">New</span></h6>
+                            <h6><span class="badge badge-secondary">New</span></h6>
+                        </div>
+                        <h6>CONNECTIONS</h6>
+                        <!-- did you want cards? -->
+                        <div class="card" style="width: 18rem;">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Computer Science</li>
+                                <li class="list-group-item">META+LAB</li>
+                                <li class="list-group-item">Advancing Technology Laboratory @Techlab</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                        
+                    <div class="col-8">
+                            <h1>Overview</h1>
+                            <p>Steven Fitzgerald is a Professor of Computer Science at California State University, Northridge, and his teaching focus is on operating systems, compilers, and networking. During his tenure, Steve has also held several administrative positions including: Information Security Officer, Director of Academic Technology, and Chief Technology Officer.</p>
+
+                            <p>Steve's research interests include high performance computing, identity management, grid computing, and cloud computing. Steve has participated in a number of research projects associated with Lawrence Livermore National Laboratory and the University of Southern California's Information Sciences Institute. During Steve's recent sabbatical, he worked for Eucalyptus Systems, a startup company producing Cloud Computing software. Eucalypus Systems was subsequenlty purchased by Hewlett Packard Enterprise, and is now refer to as HPE Helion Eucalyptus . Steve was responsible for developing and expanding their customer support services and for building out their IT infrastructure.</p>
+                            
+                            <p>In his current role, Steve also serves as the Faculty Co-Director of the Matador Emerging Technology and Arts Laboratory (META+LAB). This group is composed of a team of students, mentored by faculty members, that develop web applications and engages in other activities to improve campus services via the use of leading-edge technologies.</p>
+
+                            <h1>Team Environment</h1>
+                            <h2>Team Rules</h2>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    <footer></footer>
+</div>
+
 </template>
 
 <script>
